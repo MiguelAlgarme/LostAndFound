a744d20a401078cce463e64015fd0cdb339bf350
Bryce Nicholas B. Co <21102461@usc.edu.ph>
2023-12-23 19:39:55 +0800

    Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,51 +8,24 @@ interface FormSubmission {
   description: string;
   locationfound: string;
   datefound: string;
-  founderID: number; // Assuming there's a founderID in FormSubmission
-}
-
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
 }
 
 const Home = () => {
   const [formSubmissions, setFormSubmissions] = useState<FormSubmission[]>([]);
-  const [usersMap, setUsersMap] = useState<Record<number, User>>({});
 
   useEffect(() => {
     const fetchFormSubmissions = async () => {
       try {
         const response = await axios.get("http://localhost:3000/api/all-submissions");
         setFormSubmissions(response.data.submissions);
-  
-        // Extract founder IDs and fetch user data for each unique founder ID
-        const uniqueFounderIDs = Array.from(new Set(response.data.submissions
-          .filter(submission => submission.founderID !== undefined)
-          .map(submission => submission.founderID)
-        ));
-  
-        await Promise.all(
-          uniqueFounderIDs.map(async (founderID) => {
-            const userResponse = await axios.get(`http://localhost:3000/api/user/${founderID}`);
-            setUsersMap(prevUsersMap => ({
-              ...prevUsersMap,
-              [String(founderID)]: userResponse.data.user,
-            }));
-          })
-        );
+        console.log(response.data.submissions); // Log the received data
       } catch (error) {
         console.error("Error fetching form submissions:", error);
       }
     };
   
     fetchFormSubmissions();
-  }, []);
-  
-  
-  
+  }, []);  
 
   const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
@@ -70,12 +43,6 @@ const Home = () => {
             <p>Description: {submission.description}</p>
             <p>Location Found: {submission.locationfound}</p>
             <p>Date Found: {formatDate(submission.datefound)}</p>
-            {usersMap[submission.founderID] && (
-              <div key={submission.id}>
-                <p>Reporter: {usersMap[submission.founderID].firstName} {usersMap[submission.founderID].lastName}</p>
-                <p>Email: {usersMap[submission.founderID].email}</p>
-              </div>
-            )}
           </div>
         ))}
       </div>
